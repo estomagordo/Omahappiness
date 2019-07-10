d48061d44111d990d0d1dd0d0730e36fde3efa3d
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-07-10 11:22:24 +0200

    Suits enum

---
+++ Poker/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poker
+{
+    public class Enums
+    {
+        public enum Suits
+        {
+            Clubs,
+            Diamonds,
+            Hearts,
+            Spades
+        }
+    }
+}

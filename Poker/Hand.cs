ab123a3484cb89cd6956b2e3426c2e22654224ed
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-07-12 22:30:09 +0200

    More caching => more speedups.

--- Poker/Hand.cs
+++ Poker/Hand.cs
@@ -20,7 +20,7 @@ namespace Poker
             {
                 if (_pairCount == -1)
                 {
-                    _pairCount = _rankCounts.Values.Count(value => value == 2);
+                    _pairCount = RankCounts.Values.Count(value => value == 2);
                 }
 
                 return _pairCount;
@@ -33,36 +33,53 @@ namespace Poker
             {
                 if (_tripCount == -1)
                 {
-                    _tripCount = _rankCounts.Values.Count(value => value == 3);
+                    _tripCount = RankCounts.Values.Count(value => value == 3);
                 }
 
                 return _tripCount;
             }
         }
 
+        private List<int> OrderedKickers
+        {
+            get
+            {
+                if (_orderedKickers == null)
+                {
+                    _orderedKickers = RankCounts.Where(pair => pair.Value == 1).Select(pair => pair.Key).OrderBy(k => k).ToList();
+                }
+
+                return _orderedKickers;
+            }
+        }
+
+        private Dictionary<int, int> RankCounts
+        {
+            get
+            {
+                if (_rankCounts == null)
+                {
+                    _rankCounts = new Dictionary<int, int>();
+
+                    _cards.ForEach(card =>
+                    {
+                        if (!_rankCounts.ContainsKey(card.Rank.Value))
+                        {
+                            _rankCounts.Add(card.Rank.Value, 0);
+                        }
+
+                        _rankCounts[card.Rank.Value]++;
+                    });
+                }
+
+                return _rankCounts;
+            }
+        }
+
         public Hand(Card card0, Card card1, Card card2, Card card3, Card card4)
         {
             _cards = new List<Card>() { card0, card1, card2, card3, card4 };
-            CountRanks();
-            _cardsSortedByRank = _cards.OrderBy(card => card.Rank.Value).ToList();
-            _orderedKickers = _rankCounts.Where(pair => pair.Value == 1).Select(pair => pair.Key).OrderBy(k => k).ToList();
-        }
-
-        private void CountRanks()
-        {
-            var counts = new Dictionary<int, int>();
-
-            _cards.ForEach(card =>
-            {
-                if (!counts.ContainsKey(card.Rank.Value))
-                {
-                    counts.Add(card.Rank.Value, 0);
-                }
-
-                counts[card.Rank.Value]++;
-            });
-
-            _rankCounts = counts;
+            _cardsSortedByRank = _cards.OrderBy(card => card.Rank.Value).ToList();            
         }
 
         private bool IsOnePair()
@@ -82,7 +99,7 @@ namespace Poker
 
         private bool IsStraight()
         {
-            if (_rankCounts.Values.Any(value => value > 1))
+            if (RankCounts.Values.Any(value => value > 1))
             {
                 return false;
             }
@@ -107,7 +124,7 @@ namespace Poker
 
         private bool IsQuads()
         {
-            return _rankCounts.Values.Count(value => value == 4) == 1;
+            return RankCounts.Values.Count(value => value == 4) == 1;
         }
 
         private bool IsStraightFlush()
@@ -222,10 +239,10 @@ namespace Poker
 
             if (IsQuads())
             {
-                var thisQuadRank = _rankCounts.Where(pair => pair.Value == 4).Select(pair => pair.Key).First();
-                var thisKickerRank = _rankCounts.Where(pair => pair.Value == 1).Select(pair => pair.Key).First();
-                var otherQuadRank = other._rankCounts.Where(pair => pair.Value == 4).Select(pair => pair.Key).First();
-                var otherKickerRank = other._rankCounts.Where(pair => pair.Value == 1).Select(pair => pair.Key).First();
+                var thisQuadRank = RankCounts.Where(pair => pair.Value == 4).Select(pair => pair.Key).First();
+                var thisKickerRank = RankCounts.Where(pair => pair.Value == 1).Select(pair => pair.Key).First();
+                var otherQuadRank = other.RankCounts.Where(pair => pair.Value == 4).Select(pair => pair.Key).First();
+                var otherKickerRank = other.RankCounts.Where(pair => pair.Value == 1).Select(pair => pair.Key).First();
 
                 if (thisQuadRank == otherQuadRank)
                 {
@@ -237,10 +254,10 @@ namespace Poker
 
             if (IsFullHouse())
             {
-                var thisTripRank = _rankCounts.Where(pair => pair.Value == 3).Select(pair => pair.Key).First();
-                var thisPairRank = _rankCounts.Where(pair => pair.Value == 2).Select(pair => pair.Key).First();
-                var otherTripRank = other._rankCounts.Where(pair => pair.Value == 3).Select(pair => pair.Key).First();
-                var otherPairRank = other._rankCounts.Where(pair => pair.Value == 2).Select(pair => pair.Key).First();
+                var thisTripRank = RankCounts.Where(pair => pair.Value == 3).Select(pair => pair.Key).First();
+                var thisPairRank = RankCounts.Where(pair => pair.Value == 2).Select(pair => pair.Key).First();
+                var otherTripRank = other.RankCounts.Where(pair => pair.Value == 3).Select(pair => pair.Key).First();
+                var otherPairRank = other.RankCounts.Where(pair => pair.Value == 2).Select(pair => pair.Key).First();
 
                 if (thisTripRank == otherTripRank)
                 {
@@ -270,26 +287,26 @@ namespace Poker
 
             if (IsTrips())
             {
-                var thisTripRank = _rankCounts.Where(pair => pair.Value == 3).Select(pair => pair.Key).First();
-                var otherTripRank = other._rankCounts.Where(pair => pair.Value == 3).Select(pair => pair.Key).First();
+                var thisTripRank = RankCounts.Where(pair => pair.Value == 3).Select(pair => pair.Key).First();
+                var otherTripRank = other.RankCounts.Where(pair => pair.Value == 3).Select(pair => pair.Key).First();
 
                 if (thisTripRank != otherTripRank)
                 {
                     return thisTripRank.CompareTo(otherTripRank);
                 }
 
-                if (_orderedKickers[1] != other._orderedKickers[1])
+                if (OrderedKickers[1] != other.OrderedKickers[1])
                 {
-                    return _orderedKickers[1].CompareTo(other._orderedKickers[1]);
+                    return OrderedKickers[1].CompareTo(other.OrderedKickers[1]);
                 }
 
-                return _orderedKickers[0].CompareTo(other._orderedKickers[0]);
+                return OrderedKickers[0].CompareTo(other.OrderedKickers[0]);
             }
 
             if (IsTwoPair())
             {
-                var thisPairs = _rankCounts.Where(pair => pair.Value == 2).Select(pair => pair.Key).OrderBy(k => k).ToList();
-                var otherPairs = other._rankCounts.Where(pair => pair.Value == 2).Select(pair => pair.Key).OrderBy(k => k).ToList();
+                var thisPairs = RankCounts.Where(pair => pair.Value == 2).Select(pair => pair.Key).OrderBy(k => k).ToList();
+                var otherPairs = other.RankCounts.Where(pair => pair.Value == 2).Select(pair => pair.Key).OrderBy(k => k).ToList();
 
                 if (thisPairs[1] != otherPairs[1])
                 {
@@ -301,53 +318,53 @@ namespace Poker
                     return thisPairs[0].CompareTo(otherPairs[0]);
                 }
 
-                return _orderedKickers[0].CompareTo(other._orderedKickers[0]);
+                return OrderedKickers[0].CompareTo(other.OrderedKickers[0]);
             }
 
             if (IsOnePair())
             {
-                var thisPairRank = _rankCounts.Where(pair => pair.Value == 2).Select(pair => pair.Key).First();
-                var otherPairRank = other._rankCounts.Where(pair => pair.Value == 2).Select(pair => pair.Key).First();
+                var thisPairRank = RankCounts.Where(pair => pair.Value == 2).Select(pair => pair.Key).First();
+                var otherPairRank = other.RankCounts.Where(pair => pair.Value == 2).Select(pair => pair.Key).First();
 
                 if (thisPairRank != otherPairRank)
                 {
                     return thisPairRank.CompareTo(otherPairRank);
                 }
 
-                if (_orderedKickers[2] != other._orderedKickers[2])
+                if (OrderedKickers[2] != other.OrderedKickers[2])
                 {
-                    return _orderedKickers[2].CompareTo(other._orderedKickers[2]);
+                    return OrderedKickers[2].CompareTo(other.OrderedKickers[2]);
                 }
 
-                if (_orderedKickers[1] != other._orderedKickers[1])
+                if (OrderedKickers[1] != other.OrderedKickers[1])
                 {
-                    return _orderedKickers[1].CompareTo(other._orderedKickers[1]);
+                    return OrderedKickers[1].CompareTo(other.OrderedKickers[1]);
                 }
 
-                return _orderedKickers[0].CompareTo(other._orderedKickers[0]);
+                return OrderedKickers[0].CompareTo(other.OrderedKickers[0]);
             }
 
-            if (_orderedKickers[4] != other._orderedKickers[4])
+            if (OrderedKickers[4] != other.OrderedKickers[4])
             {
-                return _orderedKickers[4].CompareTo(other._orderedKickers[4]);
+                return OrderedKickers[4].CompareTo(other.OrderedKickers[4]);
             }
 
-            if (_orderedKickers[3] != other._orderedKickers[3])
+            if (OrderedKickers[3] != other.OrderedKickers[3])
             {
-                return _orderedKickers[3].CompareTo(other._orderedKickers[3]);
+                return OrderedKickers[3].CompareTo(other.OrderedKickers[3]);
             }
 
-            if (_orderedKickers[2] != other._orderedKickers[2])
+            if (OrderedKickers[2] != other.OrderedKickers[2])
             {
-                return _orderedKickers[2].CompareTo(other._orderedKickers[2]);
+                return OrderedKickers[2].CompareTo(other.OrderedKickers[2]);
             }
 
-            if (_orderedKickers[1] != other._orderedKickers[1])
+            if (OrderedKickers[1] != other.OrderedKickers[1])
             {
-                return _orderedKickers[1].CompareTo(other._orderedKickers[1]);
+                return OrderedKickers[1].CompareTo(other.OrderedKickers[1]);
             }
 
-            return _orderedKickers[0].CompareTo(other._orderedKickers[0]);
+            return OrderedKickers[0].CompareTo(other.OrderedKickers[0]);
         }
     }
 }
